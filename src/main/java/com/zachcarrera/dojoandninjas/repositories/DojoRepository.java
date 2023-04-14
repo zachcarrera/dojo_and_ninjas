60c45ae39fe751c9b9acc2c6558d4af187ce3c73
Zach Carrera <zachcarrera@gmail.com>
2023-04-13 21:48:40 -0700

    add DojoRepository interface

---
+++ src/main/java/com/zachcarrera/dojoandninjas/repositories/DojoRepository.java
@@ -0,0 +1,15 @@
+package com.zachcarrera.dojoandninjas.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zachcarrera.dojoandninjas.models.Dojo;
+
+@Repository
+public interface DojoRepository extends CrudRepository<Dojo, Long> {
+	
+	List<Dojo> findAll();
+
+}
